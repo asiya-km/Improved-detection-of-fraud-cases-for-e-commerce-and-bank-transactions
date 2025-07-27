78bef66685f50291fabf72198bad186c7d80b17f
asiya-km <asiyakhelifa357@gmail.com>
2025-07-27 18:59:53 +0300

    Initial commit

---
+++ notebooks/app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ed3226df-fc00-457c-b863-77c6f30d4416",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
